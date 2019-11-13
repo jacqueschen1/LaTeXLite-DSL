9b89d1a0a28da20f1638f5cd3a68ffb75629c452
Mohammad Osman <mohosman93@hotmail.com>
2019-11-12 23:03:56 -0800

    more parsed

--- LatexLite/src/ast/ENV.java
+++ LatexLite/src/ast/ENV.java
@@ -5,6 +5,16 @@ public class ENV extends LATEXLITE{
 
     public void parse(){
         tokenizer.getAndCheckNext("env");
-
+        SYMBOL sym = new SYMBOL();
+        sym.parse();
+        tokenizer.getAndCheckNext("[");
+        if (tokenizer.checkToken("var")){
+            VAR v = new VAR();
+            v.parse();
+        }
+        else if (tokenizer.checkToken("rule")){
+            RULE r = new RULE();
+            r.parse();
+        }
     }
 }
